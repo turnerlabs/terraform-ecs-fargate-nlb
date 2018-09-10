31a1ee4a2a59a6fef1616194796cf9d42d4e876c
John Ritsema <john.ritsema@turner.com>
2018-09-10 15:59:03 -0400

    moar tweaks

--- env/dev/dashboard.tf
+++ env/dev/dashboard.tf
@@ -1,32 +1,6 @@
 /**
  * This module creates a CloudWatch dashboard for you app,
- * showing its CPU and memory utilization and various HTTP-related metrics.
- *
- * The graphs of HTTP requests are stacked.  Green indicates successful hits
- * (HTTP response codes 2xx), yellow is used for client errors (HTTP response
- * codes 4xx) and red is used for server errors (HTTP response codes 5xx).
- * Stacking is used because, when things are running smoothly, those graphs
- * will be predominately green, making the dashboard easier to check
- * at a glance or at a distance.
- *
- * One of the graphs shows HTTP response codes returned by your containers.
- * Another graph shows HTTP response codes returned by your load balancer.
- * Although these two graphs often look very similar, there are situations
- * where they will differ.
- * If your containers are responding 200 OK but are taking too long to
- * respond, the load balancer will return 504 Gateway Timeout.  In that
- * case, the containers' graph could show green while the load balancer's
- * graph shows red.
- * If many of your containers are failing their healthchecks, the load
- * balancer will direct traffic to the healthy containers.  In that case,
- * the load balancer's graph could show green while the containers'
- * graph shows red.
- * The containers' graph might show more traffic than the load balancer's.
- * Some of the containers' traffic is due to the healthchecks, which
- * originate with the load balancer.  Also, it is possible that future
- * load balancers will re-attempt HTTP requests that the HTTP standard
- * declares idempotent.
- *
+ * showing its CPU and memory utilization and auto-scaling metrics.
  */
 
 resource "aws_cloudwatch_dashboard" "cloudwatch_dashboard" {
