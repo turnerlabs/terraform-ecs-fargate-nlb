9c4248ec2b6412002938a69756c39a3c067efd76
John Ritsema <john.ritsema@turner.com>
2019-08-09 13:19:11 -0400

    upgrades to new terraform v0.12 syntax (#8)

--- env/dev/nlb.tf
+++ env/dev/nlb.tf
@@ -14,37 +14,38 @@ resource "aws_lb" "main" {
   enable_cross_zone_load_balancing = "true"
 
   # launch lbs in public or private subnets based on "internal" variable
-  internal = "${var.internal}"
-  subnets  = "${local.target_subnets}"
-  tags     = "${var.tags}"
+  internal = var.internal
+  subnets  = local.target_subnets
+  tags     = var.tags
 }
 
 # adds a tcp listener to the load balancer and allows ingress
 resource "aws_lb_listener" "tcp" {
-  load_balancer_arn = "${aws_lb.main.id}"
-  port              = "${var.lb_port}"
-  protocol          = "${var.lb_protocol}"
+  load_balancer_arn = aws_lb.main.id
+  port              = var.lb_port
+  protocol          = var.lb_protocol
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.main.id}"
+    target_group_arn = aws_lb_target_group.main.id
     type             = "forward"
   }
 }
 
 resource "aws_lb_target_group" "main" {
   name                 = "${var.app}-${var.environment}"
-  port                 = "${var.lb_port}"
-  protocol             = "${var.lb_protocol}"
-  vpc_id               = "${var.vpc}"
+  port                 = var.lb_port
+  protocol             = var.lb_protocol
+  vpc_id               = var.vpc
   target_type          = "ip"
-  deregistration_delay = "${var.deregistration_delay}"
+  deregistration_delay = var.deregistration_delay
 
   health_check {
-    protocol            = "${var.lb_protocol}"
-    interval            = "${var.health_check_interval}"
+    protocol            = var.lb_protocol
+    interval            = var.health_check_interval
     healthy_threshold   = 5
     unhealthy_threshold = 5
   }
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
+
--- env/dev/nsg.tf
+++ env/dev/nsg.tf
@@ -1,21 +1,24 @@
 resource "aws_security_group" "nsg_task" {
   name        = "${var.app}-${var.environment}-task"
   description = "Limit connections from internal resources while allowing ${var.app}-${var.environment}-task to connect to all external resources"
-  vpc_id      = "${var.vpc}"
+  vpc_id      = var.vpc
 
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 # Rules for the TASK (Targets the LB's IPs)
 resource "aws_security_group_rule" "nsg_task_ingress_rule" {
   description = "Only allow connections from the NLB on port ${var.container_port}"
   type        = "ingress"
-  from_port   = "${var.container_port}"
-  to_port     = "${var.container_port}"
+  from_port   = var.container_port
+  to_port     = var.container_port
   protocol    = "tcp"
-  cidr_blocks = ["${formatlist("%s/32",flatten(data.aws_network_interface.nlb.*.private_ips))}"]
+  cidr_blocks = formatlist(
+    "%s/32",
+    flatten(data.aws_network_interface.nlb.*.private_ips),
+  )
 
-  security_group_id = "${aws_security_group.nsg_task.id}"
+  security_group_id = aws_security_group.nsg_task.id
 }
 
 resource "aws_security_group_rule" "nsg_task_egress_rule" {
@@ -26,20 +29,21 @@ resource "aws_security_group_rule" "nsg_task_egress_rule" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.nsg_task.id}"
+  security_group_id = aws_security_group.nsg_task.id
 }
 
 # lookup the ENIs associated with the NLB
 data "aws_network_interface" "nlb" {
-  count = "${length(local.target_subnets)}"
+  count = length(local.target_subnets)
 
-  filter = {
+  filter {
     name   = "description"
     values = ["ELB ${aws_lb.main.arn_suffix}"]
   }
 
-  filter = {
-    name   = "subnet-id"
-    values = ["${element(local.target_subnets, count.index)}"]
+  filter {
+    name = "subnet-id"
+    values = [element(local.target_subnets, count.index)]
   }
 }
+
